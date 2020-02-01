4aac4de9f9523b884194e64b66eb951abd240100
Михаил <shipov-mihail@mail.ru>
2020-02-02 02:04:18 +0300

    WIP: added gitignore

---
+++ Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfProject_Shipov
+{
+    class Employee
+    {
+        public string Name {get; private set; }
+    }
+}
